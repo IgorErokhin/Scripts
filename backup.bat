e634f02c50117a0cb1a3b0cb564fdf8df5a16023
Igor Eroxin <igor.gero@gmail.com>
2022-04-21 16:41:15 +0300

    Add backup script

---
+++ backup.bat
@@ -0,0 +1,1 @@
+Powershell.exe -executionpolicy remotesigned -File  "D:\Scripts\backup.ps1"
---
+++ backup.ps1
@@ -0,0 +1,7 @@
+$FoldersToBackup = "D:\Files\Notes", "D:\Files\Passwords"
+$Destination = "D:\_backup"
+$Date = Get-Date -Format "yyyyMMdd"
+Write-Host "Script to backup $FoldersToBackup to $Destination"
+New-Item -Path $Destination -Name $Date -ItemType "directory"
+Copy-Item -LiteralPath $FoldersToBackup -Destination "$Destination\$Date" -Recurse
+Write-Host "Script finished"
